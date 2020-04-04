d8341e6677e528d54b0e5be82bb7af03991136d8
Ashot Pahlevanyan <ashotpahlevanyan@gmail.com>
2020-04-04 15:29:20 +0400

    in the middle of task

--- exercises/autocomplete-4/src/task.ts
+++ exercises/autocomplete-4/src/task.ts
@@ -1,4 +1,4 @@
-import { isPromise } from './utils/promise';
+import {isPromise, wait} from './utils/promise';
 /**
  * Given a generator function that yields one or more
  * promises, chain them together in sequence
@@ -8,8 +8,36 @@ import { isPromise } from './utils/promise';
  */
 export function task<T>(genFn: () => IterableIterator<any>): Promise<T> {
   let p = new Promise<T>((resolve) => {
-    let iterator = genFn(); // Get the iterator
+    let it = genFn(); // Get the iterator
     // TODO: implement your solution here
+
+    function nextStep(lastPromiseVal: any) {
+      let itResult = it.next(lastPromiseVal);
+      if(itResult.done) {
+        console.log('done');
+        return;
+      } else {
+        let { value } = itResult;
+        if(isPromise(value)) {
+          console.log('is a promise');
+          value.then((promiseResult: any) => {
+            nextStep(promiseResult);
+          });
+        } else {
+          console.log('not a promise');
+        }
+      }
+    }
+    nextStep(undefined);
   });
   return p;
 }
+
+task(function*() {
+  yield wait(600).then(() => {
+    console.log('600 done');
+  });
+  yield wait(300).then(() => {
+    console.log('300 done');
+  });
+});
